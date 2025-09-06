bbd8e8ed0e7a3eda2142c71e08af43801ff26cc9
Мальков Олег <karielka@yandex.ru>
2025-09-06 23:28:37 +0300

    Add inverse tree

---
+++ Binary tree/226. Invert Binary Tree/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void descent(TreeNode* root) {
+        if (root == nullptr) return;
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        descent(root->left);
+        descent(root->right);
+    }
+    TreeNode* invertTree(TreeNode* root) {
+        descent(root);
+        return root;
+    }
+};
